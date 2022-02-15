b1dcb7c2e182c0dcea8a2a4bd0031f04853d5381
autoonez <autoonez@hotmail.com>
2022-02-15 14:29:09 +0700

    gh-pages

---
+++ assets/StoryReaderHome.4720e5b4.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.d14d283f.js";import"./vendor.16e44847.js";const e={};function o(t,_){return"Home"}var n=r(e,[["render",o]]);export{n as default};
--- src/constants/regex.js
+++
@@ -1,7 +0,0 @@
-export const tagRexEx = /\[(?<tag>.+?)\((?<params>.*)\)\]/;
-export const otherTagRexEx = /\[(?<tag>.*)=(?<value>.*)\]/g;
-export const blankTagRexEx = /\[(?<tag>.*)\]/;
-export const splitParamsRegEx =
-  /(?<name>\w*?)\s*=\s*(?<value>".*?"|[^",)\]\s]*)/g;
-export const characterSpeechRexEx =
-  /\[[Nn][Aa][Mm][Ee]="(?<name>.*)"(?<params>.*)\]\s*(?<text>.*)/g;
