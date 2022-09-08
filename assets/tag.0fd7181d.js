75430242b3cbcc812753db2fe57106a1b4b27227
Noppakaw Boonrasri <autoonez@hotmail.com>
2022-09-09 03:21:14 +0700

    Deploy updates

---
+++ assets/tag.0fd7181d.js
@@ -0,0 +1,1 @@
+import{g as r,p as a}from"./index.732bea17.js";function s(e){const t=r("useRender");t.render=e}const o=a({tag:{type:String,default:"div"}},"tag");export{o as m,s as u};
--- assets/tag.5404cb9c.js
+++
@@ -1,1 +0,0 @@
-import{g as r,p as a}from"./index.9343c9cb.js";function s(e){const t=r("useRender");t.render=e}const o=a({tag:{type:String,default:"div"}},"tag");export{o as m,s as u};
