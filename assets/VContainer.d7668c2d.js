3f88f28a48a09939b4ec4a053e2835d1de7c3055
Noppakaw Boonrasri <autoonez@hotmail.com>
2022-12-22 21:39:57 +0700

    Deploy updates

--- assets/VContainer.d7668c2d.js
+++
@@ -1,1 +0,0 @@
-/* empty css              */import{m as t,u as n}from"./router.d4a02ace.js";import{f as r,a as s}from"./index.039e7fc9.js";const m=r({name:"VContainer",props:{fluid:{type:Boolean,default:!1},...t()},setup(e,a){let{slots:o}=a;return n(()=>s(e.tag,{class:["v-container",{"v-container--fluid":e.fluid}]},o)),{}}});export{m as V};
